3f364b6e8bad66bd1df187242f9b960ed4c88c56
codesshaman <nicksemail@yandex.ru>
2023-05-02 18:06:45 +0300

    add actions

---
+++ game/lang/ru/level0.md
@@ -0,0 +1,6 @@
+# Level 0
+### Вспомнить всё
+comment100: Фраза 1
+command100: команда1
+success100: 
+failall100:
--- game/test.sh
+++ game/test.sh
@@ -1,15 +1,42 @@
 #!/bin/bash
-# script -q /dev/null | sed -e 's/\r//' | while read -r line; do
-#     echo "Выполнена команда: $line"
+lang="ru"
+if [ "$lang" == "ru" ]; then
+    compath="lang/ru/level0.md"
+fi
+if [ "$lang" == "en" ]; then
+    compath="lang/en/level0.md"
+fi
+compare() {
+    comment="$(grep "comment${counter}" $compath | sed -r 's/.{,11}//')"
+    command="$(grep "command${counter}" $compath | sed -r 's/.{,11}//')"
+    echo "$comment $command"
+
+}
+
+# trap "echo ' Trapped Ctrl-C'" SIGINT
+# echo This is a test script
+# count=1
+# while [ $count -le 10 ]
+# do
+# echo "Loop #$count"
+# sleep 1
+# count=$(( $count + 1 ))
+# done
+counter=100
+compare
+# trap 'echo "Enter key pressed!"' RETURN
+# count=1
+# while [ $count -le 10 ]
+# do
+# read -p "Как же меня зовут... " input
+# count=$(( $count + 1 ))
+# command="whoami"
+if [ $input == $command ]; then
+    name=$($command)
+    echo "Моё имя $name"
+else
+    name=$($input)
+    echo "$name... какое странное имя... Нет, это точно не про меня."
+fi
 # done
 
-#!/bin/bash
-trap "echo ' Trapped Ctrl-C'" SIGINT
-echo This is a test script
-count=1
-while [ $count -le 10 ]
-do
-echo "Loop #$count"
-sleep 1
-count=$(( $count + 1 ))
-done
